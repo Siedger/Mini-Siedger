64553f78d0486e3cba206a66e64ace1b7322e63d
Siedger <33349800+Siedger@users.noreply.github.com>
2017-11-03 14:52:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,34 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Fixed by RedCoder4');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message' , (message) => {
+	if (message.content == '!scrim') {
+			message.channel.send("Horarios Scrim: De Viernes a Domingos (De 4 a 8)", {
+			tts:false
+			})
+	}
+	if (message.content == '!mikel') {
+			message.channel.send("TheMaik96, mid laner del equipo: Este chico es pura skill con una goodpull bien grande. Solo el lag le para!", {
+			tts:false
+			})
+	}
+	if (message.content == '!rivas') {
+			message.channel.send("Rivasito, solo laner del equipo: Mejor jugador, streamer y de todo lo que puedas imaginar de EU y pronto mundial!", {
+			tts:false
+			})
+	}
+	if (message.content == '!adnayerc') {
+			message.channel.send("Adnayerc, support del equipo: Amante del bodyblock, gran defensor de este rol hasta la muerte!!", {
+			tts:false
+			})
+	}
+	if (message.content == '!roster') {
+			message.channel.send("Roster actual(IDs):\n\u0020\u0020\u0020\u0020-MID: TheMaik96\n\u0020\u0020\u0020\u0020-ADC: LF\n\u0020\u0020\u0020\u0020-Jungle: LF\n\u0020\u0020\u0020\u0020-Support: Adnáyerc\n\u0020\u0020\u0020\u0020-Solo: Rivasito\n\u0020\u0020\u0020\u0020-Suplentes: LF", {
+			tts: false
+			})
+	}
 });
-
-// THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
