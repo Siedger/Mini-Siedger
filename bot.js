a1b31f03e2ceb4418e044fafa9be81bedd5a1754
Siedger <33349800+Siedger@users.noreply.github.com>
2017-11-03 15:28:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,30 +9,11 @@ client.on('ready', () => {
 });
 
 client.on('message' , (message) => {
-	if (message.content == '!scrim') {
-			message.channel.send("Horarios Scrim: De Viernes a Domingos (De 4 a 8)", {
+	if (message.content == '!test') {
+			message.channel.send("Test", {
 			tts:false
 			})
 	}
-	if (message.content == '!mikel') {
-			message.channel.send("TheMaik96, mid laner del equipo: Este chico es pura skill con una goodpull bien grande. Solo el lag le para!", {
-			tts:false
-			})
-	}
-	if (message.content == '!rivas') {
-			message.channel.send("Rivasito, solo laner del equipo: Mejor jugador, streamer y de todo lo que puedas imaginar de EU y pronto mundial!", {
-			tts:false
-			})
-	}
-	if (message.content == '!adnayerc') {
-			message.channel.send("Adnayerc, support del equipo: Amante del bodyblock, gran defensor de este rol hasta la muerte!!", {
-			tts:false
-			})
-	}
-	if (message.content == '!roster') {
-			message.channel.send("Roster actual(IDs):\n\u0020\u0020\u0020\u0020-MID: TheMaik96\n\u0020\u0020\u0020\u0020-ADC: LF\n\u0020\u0020\u0020\u0020-Jungle: LF\n\u0020\u0020\u0020\u0020-Support: Adnáyerc\n\u0020\u0020\u0020\u0020-Solo: Rivasito\n\u0020\u0020\u0020\u0020-Suplentes: LF", {
-			tts: false
-			})
-	}
+
 });
 client.login(process.env.BOT_TOKEN);
